6f60616b14429aa7a5e18f68780d501aba1e47ba
bouanaoui <b.bouanaoui@gmail.com>
2017-02-23 08:48:26 +0100

    FieldSet added

--- PFE-CONIX-INFO3/app/mock.article.ts
+++ PFE-CONIX-INFO3/app/mock.article.ts
@@ -3,21 +3,21 @@ import { Article } from './article';
 export const ARTICLES :  Article[] = [
     {
         id: 0,
-        title:'BNP & Wallmart merge',
+        title:'BNP & Wallmart merge (Article neutre)',
         link:'http://myarticles/bnpwallmartfusion',
         description:'Bank merging with supermarket ?',
         score: 0
     },
     {
         id: 1,
-        title:'Total investing in IT',
+        title:'Total investing in IT (Article connoté positivement)',
         link:'http://myarticles/totalit',
         description:'Total`s OPA on IBM',
         score: 1
     },
     {
         id: 2,
-        title: 'Google`s bankrupt',
+        title: 'Google`s bankrupt (Article connoté négativement)',
         link:'http://myarticles/googlesnotsowell',
         description:'Google planning to announce bankrupt',
         score: -1
