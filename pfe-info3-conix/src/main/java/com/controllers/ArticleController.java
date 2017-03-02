c0ded217456cd407d176bc53a4d044f4c2a354e1
ashfack <abdoulazid.ashfack@gmail.com>
2017-03-02 14:27:06 +0100

    Methode dashboard dans le Back pour récupérer la tendance des entreprises du CAC 40

--- pfe-info3-conix/src/main/java/com/controllers/ArticleController.java
+++ pfe-info3-conix/src/main/java/com/controllers/ArticleController.java
@@ -1,5 +1,7 @@
 package com.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.models.Article;
 import com.models.Extractor;
 import com.services.ArticleService;
 
@@ -37,6 +40,32 @@ public class ArticleController
 			ex.extract(firmName);
 		});
 		logger.info("Mise à jour effectuée");
+	}
+	
+	/* NomEntreprise, NbArticles positifs - NbArticles negatifs */
+	@RequestMapping(value = "/dashboard/", method = RequestMethod.GET, produces = "application/json")
+	public Map<String, Double> dashBoard() 
+	{
+		Extractor ex = new Extractor();
+		ex.setArticleService(this.articleService);
+		Map<String, String> mapEntreprises = ex.getMapEntreprises();
+		/* NomEntreprise, NbArticles positifs - NbArticles negatifs */
+		Map<String, Double> mapResults = new HashMap<String, Double>();
+		
+		mapEntreprises.forEach((firmName, sector) -> 
+		{
+			ArrayList<Article> articles;
+			logger.info("Processing " + firmName);
+			articles = ex.extract(firmName);
+			double tendance = 0;
+			for (Article article : articles) 
+			{
+				tendance += article.getScore();
+			}
+			mapResults.put(firmName, tendance);
+		});
+		logger.info("Dashboard prêt");
+		return mapResults;
 	}
 	// TODO: Rajouter une méthode clean pour remettre à zéro la BDD
 }
